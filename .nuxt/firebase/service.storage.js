8eb377a013504c6e2a33bbd0aadf22c7f4db3d23
LAPTOP-2FR6Q153\Parloppap <pimpatipan@punchup.world>
2021-10-22 15:04:42 +0700

    add npm config set

--- .nuxt/firebase/service.storage.js
+++ .nuxt/firebase/service.storage.js
@@ -1,7 +1,7 @@
 
 
 export default async function (session) {
-  await import(/* webpackChunkName: 'firebase-storage' */'firebase/storage')
+  await import('firebase/storage')
 
   const storageService = session.storage()
 
