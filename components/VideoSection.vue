fe64d267f98080147f5b485547dd873c2f47427e
LAPTOP-2FR6Q153\Parloppap <pimpatipan@punchup.world>
2021-10-26 14:20:04 +0700

    .gitignore add .nuxt

--- components/VideoSection.vue
+++ components/VideoSection.vue
@@ -15,38 +15,56 @@
       <div class="videos mt-5">
         <div class="d-flex justify-content-center align-items-center mb-3">
           <video width="320" height="240" controls :poster="video_pic_1">
-            <source :src="video_1" type="video/mp4" />
+            <source
+              src="https://d208eq9ndr4893.cloudfront.net/video_1.mp4"
+              type="video/mp4"
+            />
             Your browser does not support the video tag.
           </video>
           <div class="w-25 p-5">
-            <p class="header-3">
-              กรุงเทพมหานครมองเรื่องการมีส่วนร่วม
-            </p>
+            <p class="header-3">กรุงเทพมหานครมองเรื่องการมีส่วนร่วม</p>
             <span class="w-50 text-1">
-         การจะสร้างกลไกการมีส่วนร่วมให้มากที่สุด ต้องเป็นกลไกที่ทำให้เกิดความรับผิดชอบ โดยถ้ามีกำหนดให้ใช้จ่ายเงิน รัฐจะเป็นคนใช้จ่ายเงินลงความต้องการของพื้นใดพื้นที่นึง และพื้นที่นั้นต้องเสียสละเวลามาให้ข้อมูล ช่วยให้ความเห็นกำกับติดตาม เป็นเชิงสองฝ่ายทำงานร่วมกัน และการกำหนดกลไกงบประมาณแบบมีส่วนร่วม ช่วยหนุนให้แต่ละฝ่ายมีความสม่ำเสมอในการร่วมมือ และต่องบฯ ทำให้กลไกมีสลักสำคัญขึ้นจึงนำไปอยู่ในแผน
+              การจะสร้างกลไกการมีส่วนร่วมให้มากที่สุด
+              ต้องเป็นกลไกที่ทำให้เกิดความรับผิดชอบ โดยถ้ามีกำหนดให้ใช้จ่ายเงิน
+              รัฐจะเป็นคนใช้จ่ายเงินลงความต้องการของพื้นใดพื้นที่นึง
+              และพื้นที่นั้นต้องเสียสละเวลามาให้ข้อมูล
+              ช่วยให้ความเห็นกำกับติดตาม เป็นเชิงสองฝ่ายทำงานร่วมกัน
+              และการกำหนดกลไกงบประมาณแบบมีส่วนร่วม
+              ช่วยหนุนให้แต่ละฝ่ายมีความสม่ำเสมอในการร่วมมือ และต่องบฯ
+              ทำให้กลไกมีสลักสำคัญขึ้นจึงนำไปอยู่ในแผน
             </span>
           </div>
         </div>
 
         <div class="d-flex justify-content-center align-items-center mb-3">
           <video width="320" height="240" controls :poster="video_pic_2">
-            <source :src="video_2" type="video/mp4" />
+            <source
+              src="https://d208eq9ndr4893.cloudfront.net/video_2.mp4"
+              type="video/mp4"
+            />
             Your browser does not support the video tag.
           </video>
           <div class="w-25 p-5">
-            <p class="header-3">จุดเริ่มต้นงบประมาณ
-แบบมีส่วนร่วมในแผน
-พัฒนากรุงเทพมหานคร?
-</p>
-              <span class="w-50 text-1">
-    ปกติกรุงเทพฯ โดยเฉพาะสำนักงานเขตอาศัยความคิดเห็นจากประชาชนทำงาน ต่อไปในช่วงปี 2566 -2570 จะเป็นช่วงเน้นสร้างกลไกเครือข่ายภาคประชาสังคมในการเปิดพื้นที่รับฟังความคิดเห็น ระบบงบประมาณแบบมีส่วนร่วม คือ สำนักเขตจะทำหน้าที่จัดทำแผนพัฒนาเขตที่มาจากการมีส่วนร่วมของตัวชุมชนนำเทคโนโลยีมาช่วย และเน้นการเปิดเผยข้อมูลไปพร้อมกัน  
+            <p class="header-3">
+              จุดเริ่มต้นงบประมาณ แบบมีส่วนร่วมในแผน พัฒนากรุงเทพมหานคร?
+            </p>
+            <span class="w-50 text-1">
+              ปกติกรุงเทพฯ โดยเฉพาะสำนักงานเขตอาศัยความคิดเห็นจากประชาชนทำงาน
+              ต่อไปในช่วงปี 2566 -2570
+              จะเป็นช่วงเน้นสร้างกลไกเครือข่ายภาคประชาสังคมในการเปิดพื้นที่รับฟังความคิดเห็น
+              ระบบงบประมาณแบบมีส่วนร่วม คือ
+              สำนักเขตจะทำหน้าที่จัดทำแผนพัฒนาเขตที่มาจากการมีส่วนร่วมของตัวชุมชนนำเทคโนโลยีมาช่วย
+              และเน้นการเปิดเผยข้อมูลไปพร้อมกัน
             </span>
           </div>
         </div>
 
         <div class="d-flex justify-content-center align-items-center">
-                 <video width="320" height="240" controls :poster="video_pic_3">
-            <source :src="video_3" type="video/mp4" />
+          <video width="320" height="240" controls :poster="video_pic_3">
+            <source
+              src="https://d208eq9ndr4893.cloudfront.net/video_3.mp4"
+              type="video/mp4"
+            />
             Your browser does not support the video tag.
           </video>
           <div class="w-25 p-5">
@@ -54,7 +72,12 @@
               ประเทศไทย หากจะไปสู่ งบประมาณแบบมีส่วนร่วมได้ ควรเริ่มจากตรงไหน?
             </p>
             <span class="w-50 text-1">
-          ต้องมีระบบ monitoring and Evaluation ในการใช้งบประมาณเพื่อใช้ในการตัดสินใจเชิงยุทธศาสตร์ ไม่ใช่แบบราย item วัตถุประสงค์ในการทำระบบนี้คือ สามารถช่วยในการปรับตัวและความก้าวหน้าเกิดขึ้น ข้อผิดพลาดช่วยกันแก้ไข และได้เรียนรู้ในด้านที่จะพัฒนาผ่านกลไกแบบมีส่วนร่วมว่าสถาปนาขึ้นได้
+              ต้องมีระบบ monitoring and Evaluation
+              ในการใช้งบประมาณเพื่อใช้ในการตัดสินใจเชิงยุทธศาสตร์ ไม่ใช่แบบราย
+              item วัตถุประสงค์ในการทำระบบนี้คือ
+              สามารถช่วยในการปรับตัวและความก้าวหน้าเกิดขึ้น
+              ข้อผิดพลาดช่วยกันแก้ไข
+              และได้เรียนรู้ในด้านที่จะพัฒนาผ่านกลไกแบบมีส่วนร่วมว่าสถาปนาขึ้นได้
             </span>
           </div>
         </div>
@@ -72,10 +95,7 @@ export default {
       illus_section_end: require("~/assets/images/illus_section_end.png"),
       video_pic_1: require("~/assets/images/cover_vdo/ELECT_Graphic_VDO-01.png"),
       video_pic_2: require("~/assets/images/cover_vdo/ELECT_Graphic_VDO-04.png"),
-      video_pic_3: require("~/assets/images/cover_vdo/ELECT_Graphic_VDO-10.png"),
-      video_1: require("~/assets/video/video_1.mp4"),
-      video_2: require("~/assets/video/video_2.mp4"),
-      video_3: require("~/assets/video/video_3.mp4"),
+      video_pic_3: require("~/assets/images/cover_vdo/ELECT_Graphic_VDO-10.png")
     };
   },
   methods: {},
