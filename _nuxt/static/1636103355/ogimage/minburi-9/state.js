55e175b7c53a37446ca55d596631f2380c31ced1
Pimpatipan <Pimpatipan@users.noreply.github.com>
2021-11-05 09:10:50 +0000

    deploy: 695f1d4b49147970bd67f0f29d134c0b6c94642f

---
+++ _nuxt/static/1636103355/ogimage/minburi-9/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636103355",layout:"default",error:a,state:{district:"คลองสาน",problem:"ในการจัดการขยะมากขึ้น",isUpCountry:false},serverRendered:true,routePath:"\u002Fogimage\u002Fminburi-9",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
